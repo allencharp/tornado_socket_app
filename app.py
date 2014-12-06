bcc9d8d9829907f2ae94de79f7ec9115710c30a2
allencharp <liuzhidong_1985@hotmail.com>
2014-12-06 22:27:18 +0800

    q :q

--- app.py
+++ app.py
@@ -8,55 +8,58 @@ buffers = []
 
 
 class IndexHandler(tornado.web.RequestHandler):
-    @tornado.web.asynchronous
-    def get(request):
-        request.render("index.html")
+	@tornado.web.asynchronous
+	def get(self, request):
+		request.render("index.html")
 
 
 class SocketChatHandler(tornado.websocket.WebSocketHandler):
-    def open(self, *args):
-        print("open", "WebSocketChatHandler")
-        clients.append(self)
+	def open(self, *args):
+		print("open", "WebSocketChatHandler")
+		clients.append(self)
 
-    def on_message(self, message):
-        print message
-        for client in clients:
-            client.write_message(message)
-
-    def on_close(self):
-        clients.remove(self)
+	def on_message(self, message):
+		print message
+		for client in clients:
+			client.write_message(message)
+	
+	def on_close(self):
+		clients.remove(self)
 
 
 class UploadHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.render("upload.html")
+	def get(self):
+		self.render("upload.html")
 
 
 class SocketUploadHandler(tornado.websocket.WebSocketHandler):
-    def open(self, *args):
-        pass
+	#hello
+	def open(self, *args):
+		pass
 
-    def on_message(self, message):
-        if buffers == []:
-            buffers.append(message)
-        else:
-            buffers.append(message)
-            import os
-            file_path = os.path.dirname(__file__) + buffers[0]
-            bin_file = open(file_path, 'wb')
-            bin_file.write(buffers[1])
-            bin_file.close()
-            buffers.pop()
-            buffers.pop()
+	def on_message(self, message):
+		if buffers == []:
+			print "start"
+			buffers.append(message)
+		else:
+			buffers.append(message)
+			import os
+			file_path = os.path.dirname(__file__) + buffers[0]
+			print file_path
+			bin_file = open(file_path, 'wb')
+			bin_file.write(buffers[1])
+			bin_file.close()
+			buffers.pop()
+			buffers.pop()
 
-    def on_close(self):
-        pass
+	def on_close(self):
+		pass
 
 
 app = tornado.web.Application([(r'/chat', SocketChatHandler),
-                               (r'/upload', UploadHandler),
-                               (r'/socket_upload', SocketUploadHandler),
-                               (r'/', IndexHandler)],
-                              debug=True)
+							(r'/upload', UploadHandler),
+							(r'/socket_upload', SocketUploadHandler),
+							(r'/', IndexHandler)],
+							debug=True)
 app.listen(8005)
 tornado.ioloop.IOLoop.instance().start()
