5af47d24c6ed006225d5945af4318f1e663a6c5b
allencharp <liuzhidong_1985@hotmail.com>
2014-12-07 15:36:14 +0800

    diff

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ import tornado.websocket
 clients = []
 buffers = []
 
+#diff
 class IndexHandler(tornado.web.RequestHandler):
 	@tornado.web.asynchronous
 	def get(self, request):
 		request.render("index.html")
-
 
 class SocketChatHandler(tornado.websocket.WebSocketHandler):
 	def open(self, *args):
